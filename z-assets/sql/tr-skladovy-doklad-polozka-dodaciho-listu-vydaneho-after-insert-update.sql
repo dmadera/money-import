bd5751b2eb70b3a62aee2ba9fd86b98bee9b6421
Daniel Maděra <madera.dan@gmail.com>
2021-03-12 16:07:56 +0100

    WIP

---
+++ z-assets/sql/tr-skladovy-doklad-polozka-dodaciho-listu-vydaneho-after-insert-update.sql
@@ -0,0 +1,28 @@
+USE [S4_Agenda_PEMA]
+GO
+
+CREATE OR ALTER TRIGGER TR_SkladovyDoklad_PolozkaDodacihoListuVydaneho_AfterInsert
+ON SkladovyDoklad_PolozkaDodacihoListuVydaneho
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+	
+	/* 
+		MnozstviPozn_UserData:
+		* poslední kus
+		- odbìr do mínusu
+	*/
+	UPDATE SkladovyDoklad_PolozkaDodacihoListuVydaneho SET
+		MnozstviPozn_UserData = (CASE
+			WHEN Zas.DostupneMnozstvi = 0 THEN '*'
+			WHEN Zas.DostupneMnozstvi < 0 THEN '-'
+			ELSE ''
+		END)
+	FROM SkladovyDoklad_PolozkaDodacihoListuVydaneho AS Pol
+	INNER JOIN inserted ON inserted.ID = Pol.ID
+	INNER JOIN Obchod_ObsahPolozkySArtiklem AS ObP ON ObP.ID = Pol.ObsahPolozky_ID
+	INNER JOIN Sklady_Zasoba AS Zas ON Zas.ID = ObP.Zasoba_ID
+
+	SET NOCOUNT OFF;
+END
